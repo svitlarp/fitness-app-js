f274ad1ec62d4750b9110753ccc6ca36f7abf764
svitlarp <svitlanka.ripa@gmail.com>
2025-07-07 15:46:17 +0300

    initial commit, base structure

---
+++ vite.config.js
@@ -0,0 +1,54 @@
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
+
+
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    envDir: '../',
+    build: {
+      sourcemap: true,
+      assetsInlineLimit: 0, // Disable inlining of assets
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            if (assetInfo.name && /\.webp$/.test(assetInfo.name)) {
+              return 'assets/[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      },
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html', './src/**/**.js', './src/**/**.css']),
+    //   SortCss({
+    //     sort: 'mobile-first',
+    //   }),
+    ],
+  };
+});
